d15d63936ee81224f87255f8ada2ebbfad5e88ca
tlarican <46582411+tlarican@users.noreply.github.com>
2019-04-15 11:07:12 -0700

    Removes header row
    Took 1 minute

--- Parser.py
+++ Parser.py
@@ -5,3 +5,5 @@ with open('Video_Game_Sales_as_of_Jan_2017.csv') as csvfile:
     reader = csv.reader(csvfile, quoting=csv.QUOTE_ALL)
     for row in reader:
         results.append(row)
+
+results.pop(0)
